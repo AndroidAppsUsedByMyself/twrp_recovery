6dcbc2250d14239e2158694ad81d1cd7630a8017
sekaiacg <sekaiacg@gmail.com>
2023-08-29 22:00:13 +0800

    twrp-functions: Add File_Exists function
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- twrp-functions.hpp
+++ twrp-functions.hpp
@@ -76,6 +76,7 @@ public:
 	static int32_t timespec_diff_ms(timespec& start, timespec& end);            // Returns diff in ms
 	static bool Wait_For_File(const string& path, std::chrono::nanoseconds timeout); // Wait For File, True is success, False is timeout;
 	static bool Wait_For_Battery(std::chrono::nanoseconds timeout);             // Wait For /sys/class/power_supply/battery or TW_CUSTOM_BATTERY_PATH, True is success, False is timeout;
+	static bool File_Exists(const string& path);                                // File exists
 
 #ifndef BUILD_TWRPTAR_MAIN
 	static int Recursive_Mkdir(string Path);                                    // Recursively makes the entire path
